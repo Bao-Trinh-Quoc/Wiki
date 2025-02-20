58bde93080b4667bc377748110dd283f9fbb29be
Bao-Trinh-Quoc <bao.trinhquoc@hcmut.edu.vn>
2025-02-20 22:09:01 +0700

    docs: Add comments to improve code clarity

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -16,7 +16,9 @@ def entries(request, title):
         return render(request, "encyclopedia/error.html", {
             "title": title
         })
+    # convert markdown to html
     content_html = markdown2.markdown(util.get_entry(title))
+
     return render(request, "encyclopedia/entries.html", {
         "title": title,
         "content": content_html
@@ -25,12 +27,13 @@ def entries(request, title):
 def search(request):
     query = request.GET.get('q', '')
 
+    # If the query is an exact match of an entry title
     if util.get_entry(query):
         return redirect('entries', title=query)
-    else: 
+    else: # if the query is a substring of an entry title
         list_entries = util.list_entries()
         results = [entry for entry in list_entries if query.lower() in entry.lower()]
-
+        # return the relevant entries
         return render(request, "encyclopedia/search_results.html", {
             "query": query,
             "results": results
@@ -39,9 +42,10 @@ def search(request):
 def create(request):
     # when user click create button
     if request.method == "POST":
+        # get title and content from the form data
         title = request.POST.get("title")
         content = request.POST.get("content")
-
+        # if entry already exists
         if util.get_entry(title):
             return render(request, "encyclopedia/create.html", {
                 "error": "Entry already exists"
